1c94dd300b6b0e80e80045028868096c17a0d8ba
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-11 01:17:10 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,19 @@
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def valid_move?(board, index)
+
+  if index.between(1,9) == false
+    return false
+    break
+  elsif board[index] == "X" || "O"
+    return false
+    break
+  end
+end
+    
