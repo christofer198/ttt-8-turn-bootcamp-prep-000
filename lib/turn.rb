dc85b6b5bc831e3753ad7e0e7f4d2ef4a81af8b1
Christofer Johnson <christofer.d.johnson@gmail.com>
2017-09-11 01:32:04 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,9 +7,9 @@ def display_board(board)
     puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(index-1)
+def input_to_index(index)
 
-  index.to_i
+  index.to_i - 1
 
 end
 
